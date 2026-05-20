30bf7de7b81d4dbe4c33e2421e24c460bd7447b7
alexeyvit2697 <alexeyvit2697@gmail.com>
2026-05-20 15:11:36 +0200

    Update routes.js

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ const routes = {
         lat: 44.8234,
         lng: 20.4489,
         radius: 180,
-        audio: "audio/sunset/01-intro.mp3",
+        audio: "audio/sunset/01-intro.m4a",
         note: "Включить приветственный трек, когда гости уже на борту."
       },
       {
